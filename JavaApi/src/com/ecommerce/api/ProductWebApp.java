03307c7011f0b6cf8eee0a275d9b1f0c39374aa5
Nitija-04 <nitija.anil@gmail.com>
2026-02-11 16:17:20 +0530

    Fix port for cloud deployment

--- JavaApi/src/com/ecommerce/api/ProductWebApp.java
+++ JavaApi/src/com/ecommerce/api/ProductWebApp.java
@@ -23,7 +23,10 @@ public class ProductWebApp {
 
     
     public void start() throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        String portEnv = System.getenv("PORT");
+int port = (portEnv != null) ? Integer.parseInt(portEnv) : 8080;
+HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
 
        
         server.createContext("/", new WebUIHandler());
